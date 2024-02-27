c18b809918521b6b35dec087b1e631eaf14b0bbc
rigozdev <rodrigo.gonzaleztr@outlook.com>
2024-02-27 12:21:21 -0300

    agrega data-mock y carpeta de estilos, se modifica/crea galeria y filtro

---
+++ src/assets/data-mock/usuarios.js
@@ -0,0 +1,115 @@
+const usuarios = [
+    {
+        usuario_id: 1,
+        usuario_nombre: "Jose",
+        usuario_apellido: "Pinto",
+        contrasenha: "password123",
+        rol_usuario: 2,
+        email: "josepinto@gmail.com",
+        fecha_creacion: "02-10-2024",
+        nombre_empresa: "Sodimac",
+        telefono_empresa: "+56944444444"
+    },
+    {
+        usuario_id: 2,
+        usuario_nombre: "Maria",
+        usuario_apellido: "Gonzalez",
+        contrasenha: "maria123",
+        rol_usuario: 3,
+        email: "mariagonzalez@gmail.com",
+        fecha_creacion: "05-10-2024",
+        nombre_empresa: "Falabella",
+        telefono_empresa: "+56955555555"
+    },
+    {
+        usuario_id: 3,
+        usuario_nombre: "Pedro",
+        usuario_apellido: "Lopez",
+        contrasenha: "pedro123",
+        rol_usuario: 1,
+        email: "pedrolopez@gmail.com",
+        fecha_creacion: "08-10-2024",
+        nombre_empresa: "Ripley",
+        telefono_empresa: "+56966666666"
+    },
+    {
+        usuario_id: 4,
+        usuario_nombre: "Ana",
+        usuario_apellido: "Sanchez",
+        contrasenha: "ana123",
+        rol_usuario: 2,
+        email: "anasanchez@gmail.com",
+        fecha_creacion: "11-10-2024",
+        nombre_empresa: "Sodimac",
+        telefono_empresa: "+56977777777"
+    },
+    {
+        usuario_id: 5,
+        usuario_nombre: "Juan",
+        usuario_apellido: "Perez",
+        contrasenha: "juan123",
+        rol_usuario: 3,
+        email: "juanperez@gmail.com",
+        fecha_creacion: "14-10-2024",
+        nombre_empresa: "Falabella",
+        telefono_empresa: "+56988888888"
+    },
+    {
+        usuario_id: 6,
+        usuario_nombre: "Laura",
+        usuario_apellido: "Rodriguez",
+        contrasenha: "laura123",
+        rol_usuario: 1,
+        email: "laurarodriguez@gmail.com",
+        fecha_creacion: "17-10-2024",
+        nombre_empresa: "Ripley",
+        telefono_empresa: "+56999999999"
+    },
+    {
+        usuario_id: 7,
+        usuario_nombre: "Carlos",
+        usuario_apellido: "Garcia",
+        contrasenha: "carlos123",
+        rol_usuario: 2,
+        email: "carlosgarcia@gmail.com",
+        fecha_creacion: "20-10-2024",
+        nombre_empresa: "Sodimac",
+        telefono_empresa: "+56911111111"
+    },
+    {
+        usuario_id: 8,
+        usuario_nombre: "Sofia",
+        usuario_apellido: "Martinez",
+        contrasenha: "sofia123",
+        rol_usuario: 3,
+        email: "sofiamartinez@gmail.com",
+        fecha_creacion: "23-10-2024",
+        nombre_empresa: "Falabella",
+        telefono_empresa: "+56922222222"
+    },
+    {
+        usuario_id: 9,
+        usuario_nombre: "Diego",
+        usuario_apellido: "Fernandez",
+        contrasenha: "diego123",
+        rol_usuario: 1,
+        email: "diegofernandez@gmail.com",
+        fecha_creacion: "26-10-2024",
+        nombre_empresa: "Ripley",
+        telefono_empresa: "+56933333333"
+    },
+    {
+        usuario_id: 10,
+        usuario_nombre: "Camila",
+        usuario_apellido: "Torres",
+        contrasenha: "camila123",
+        rol_usuario: 2,
+        email: "camilatorres@gmail.com",
+        fecha_creacion: "29-10-2024",
+        nombre_empresa: "Sodimac",
+        telefono_empresa: "+56944444444"
+    }
+];
+
+// Mostrar el arreglo en la consola
+console.log(arreglo);
--- src/pages/Publicaciones.jsx
+++ src/pages/Publicaciones.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import "../pages/styles/Publicaciones.css"
+
+
+
 
 const Publicaciones = () => {
+    //hooks 
+    const [publicacion, setPublicacion] = useState("");
+
+
     return (
-        <div>Publicaciones</div>
-    )
+        <>
+            {/* Img de publicación Galera */}
+            <article className="filtro-container">
+                <h5 className="filtro-text">Filtro:</h5>
+
+                <select className="orden-selector" name="" id="">
+                    <option value="">Precio Mayor - Menor</option>
+                    <option value="">Precio Menor - Mayor</option>
+                    <option value="">Cantidad Mayor - Menor</option>
+                    <option value="">Cantaidad Menor - Mayor</option>
+
+                </select>
+                {/* <label htmlFor=""></label> */}
+                <input type="text" placeholder="Buscar publicación" />
+            </article>
+
+
+            <section className="main-section">
+                <article className="galeria-publicaciones">
+
+                    {/* (esto va en componente card) */}
+                    <div className="publicacion-container">
+                        <div className="img-container">
+                            <img src="/src/assets/data-mock/img-kita/reja_2.jpg" alt="" />
+                        </div>
+                        <div className="body-card-container">
+                            <h5 className="title-card">Titulo Publicacion</h5>
+                            <hr />
+                            <p className="desc-card"><b>Usuario:</b></p>
+                            <p className="desc-card">Precio: <b>$</b></p>
+                            <p className="desc-card"><b>Comuna:</b></p>
+                        </div>
+                    </div>
+                </article>
+            </section>
+        </>)
 }
 
 export default Publicaciones
