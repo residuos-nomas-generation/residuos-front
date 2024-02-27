711749dd9e58ea157a128f809daa14fed180e21e
rigozdev <rodrigo.gonzaleztr@outlook.com>
2024-02-27 10:30:49 -0300

    feat: se agregan más rutas, se incorpora pagina de contacto, navbar con rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import Publicaciones from "./pages/Publicaciones";
+import AcercaDe from "./pages/AcercaDe";
+import InfoContacto from "./pages/InfoContacto";
 
 const App = () => {
   return (
@@ -13,7 +16,9 @@ const App = () => {
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/*" element={<NotFound />} />
-
+          <Route path="/tienda" element={<Publicaciones />} />
+          <Route path="/nosotros" element={<AcercaDe />} />
+          <Route path="/info" element={<InfoContacto />} />
         </Routes>
       </main>
     </>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import "./navBar.css";
 import logo from '../../assets/logo.png';
 
@@ -27,14 +28,19 @@ const NavBar = () => {
     <div className="container-navbar">
       <div className="navbar align-content-center ">
         <div className="nav-logo d-flex align-items-center gap-2 m-3">
-          <img src={logo} className="nav-logo-img" alt="Logo" />
-          <p>R e s i d u o s <br />N o M á s</p>
+          <NavLink to="/" className="nav-logo d-flex align-items-center gap-2 m-3">
+            <img src={logo} className="nav-logo-img" alt="Logo" />
+            <p>R e s i d u o s <br />N o M á s</p>
+          </NavLink>
+
         </div>
         <ul className={`nav-menu d-flex align-items-center ${isMenuVisible ? 'nav-menu-visible' : 'nav-menu-hidden'}`}>
-          <li className="menu-item"><a href="#">T i e n d a</a></li>
-          <li><a href="#">N o s o t r o s</a></li>
-          <li><a href="#">C o n t a c t o</a></li>
-          <li><a href='#' className="btn btn-custom d-flex align-items-center">A C C E D E R / R E G I S T R A R S E</a></li>
+
+          {/* ! tienda */}
+          <li className="menu-item"><NavLink to="/tienda">T i e n d a</NavLink></li>
+          <li><NavLink to="/nosotros">N o s o t r o s</NavLink></li>
+          <li><NavLink to="/info">C o n t a c t o</NavLink></li>
+          <li><NavLink to="/login" className="btn btn-custom d-flex align-items-center">A C C E D E R / R E G I S T R A R S E</NavLink></li>
         </ul>
         <div className="nav-toggle" aria-label={isMenuVisible ? "Cerrar menú" : "Abrir menú"} onClick={toggleMenu}>
           <i className="uil uil-bars" />
